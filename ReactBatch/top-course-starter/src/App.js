e2d64385cd373314d80506e241de00089a93ebe6
Pradeepkumar Pal <pradeep2904pal@gmail.com>
2024-09-04 19:39:49 +0530

    React Top-course

--- ReactBatch/top-course-starter/src/App.js
+++ ReactBatch/top-course-starter/src/App.js
@@ -1,14 +1,50 @@
 import React, { useEffect, useState } from "react";
+import { ToastContainer, toast } from 'react-toastify';
 import Navbar from './components/Navbar'
 import Filters from './components/Filters'
 import Cards from './components/Cards'
+import { apiUrl, filterData } from "./data";
+import Spinner from "./components/Spinner"
 
 const App = () => {
+  const [courses, setCourses] = useState([]);
+  const [loading, setLoading] = useState(true);
+  const [category, setCategory] = useState(filterData[0].title);
+
+  async function fetchData() {
+    setLoading(true);
+    try {
+      let response = await fetch(apiUrl);
+      let output = await response.json();
+      // output ->
+      setCourses(output.data);
+    } catch (error) {
+      toast.error("Network me koi dikkat hai");
+    }
+
+    setLoading(false);
+  }
+
+  useEffect(() => {
+    fetchData();
+  }, [])
 
   return (<div>
-      <Navbar />
-      <Filters />
-      <Cards />
+    <div className="min-h-screen flex flex-col">
+      <div>
+        <Navbar />
+      </div>
+      <div className="bg-bgDark2">
+        <div>
+          <Filters category={category} setCategory={setCategory} filterData={filterData} />
+        </div>
+        <div className="w-11/12 max-w-[1200px] mx-auto flex flex-wrap justify-center min-h-[50vh]">
+          {
+            loading ? (<Spinner />) : (<Cards courses={courses} />)
+          }
+        </div>
+      </div>
+    </div>
   </div>);
 };
 
--- ReactBatch/top-course-starter/src/components/Cards.js
+++ ReactBatch/top-course-starter/src/components/Cards.js
@@ -1,9 +1,28 @@
+import React, { useEffect, useState } from "react";
+import Card from './Card';
 
+const Cards = (props) => {
+    let courses = props.courses;
+    const [likedCourses, setLikedCourses] = useState([]);
 
-const Cards = () => {
+    function getCourses() {
+        let allCourses = [];
+        Object.values(courses).forEach(array => {
+            array.forEach(courseData => {
+                allCourses.push(courseData);
+            })
+        })
+
+        return allCourses;
+    }
+
     return (
-        <div>
-
+        <div className="flex flex-wrap justify-center gap-4 mb-4">
+            {
+                getCourses().map((course) => (
+                    <Card key={course.id} course={course} likedCourses={likedCourses} setLikedCourses={setLikedCourses} />
+                ))
+            }
         </div>
     )
 }
